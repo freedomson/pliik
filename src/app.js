34552410fb3c7f3e91883d296c078077c028692b
Philip <philip@allegria2>
2012-04-12 18:30:53 +0100

    initial upload

---
+++ src/app.js
@@ -0,0 +1,17 @@
+var couchapp = require('couchapp'), 
+path = require('path');
+
+ddoc = 
+{
+    _id:'_design/p3', 
+    rewrites : [ {
+        from:"/", 
+        to:'index.html'
+    }]
+};
+
+
+
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'app'));
+
+module.exports = ddoc;
---
+++ src/push.sh
@@ -0,0 +1,1 @@
+node node_modules/.bin/couchapp push app.js http://XXX:XXX@www.pliik.com/dev
