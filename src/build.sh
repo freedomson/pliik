372b8d0fdbac0b4f6a628baf06d8f862c006e508
philip <philip@fantastic.(none)>
2012-05-09 00:53:57 +0100

    builded ;)

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,3 @@
 node node_modules/requirejs/bin/r.js -o app.build.js
+node node_modules/uglify-js/bin/uglifyjs -nm -nmf -ns --no-seqs --no-dead-code -nc -o build/js/main.js --overwrite build/js/main.js
+node node_modules/uglify-js/bin/uglifyjs -nm -nmf -ns --no-seqs --no-dead-code -nc -o build/js/libs/vendor/require/require.js --overwrite build/js/libs/vendor/require/require.js
