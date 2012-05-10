a45dd2f4593fc92ee861457f5d1ba1977b75f662
Philip <philip@allegria2>
2012-05-10 17:06:57 +0100

    Developer Guide

---
+++ src/app/tutorial/pliik-developer-guide/build.sh
@@ -0,0 +1,5 @@
+rm -rf html
+pdflatex -output-directory=pdf pliik-developer-guide.tex
+latexml --dest=html/pliik-developer-guide.xml pliik-developer-guide.tex
+#latexmlpost --nodefaultcss --dest=html/pliik-developer-guide.html html/pliik-developer-guide.xml
+latexmlpost --dest=html/index.html html/pliik-developer-guide.xml
