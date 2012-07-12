fac028fdb71746e01fc924013043507e8fa8ab47
phil <philip.almeida@freedomson.com>
2012-07-12 13:31:47 +0100

    tutorial

--- src/app/tutorial/pliik-developer-guide/build.sh
+++ src/app/tutorial/pliik-developer-guide/build.sh
@@ -2,4 +2,4 @@ rm -rf html
 pdflatex -output-directory=pdf pliik-developer-guide.tex
 latexml --dest=html/pliik-developer-guide.xml pliik-developer-guide.tex
 #latexmlpost --nodefaultcss --dest=html/pliik-developer-guide.html html/pliik-developer-guide.xml
-latexmlpost --dest=html/index.html html/pliik-developer-guide.xml
+latexmlpost --novalidate --css=style.css --dest=html/index.html html/pliik-developer-guide.xml
